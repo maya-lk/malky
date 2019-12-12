615c5ee0b98f706f7718871738cfc6f1d6191f57
hettipower <hettipower@gmail.com>
2019-12-12 13:56:03 +0530

    Reservation summary component updated

--- src/components/reservation-summery/reservation-summery.component.jsx
+++ src/components/reservation-summery/reservation-summery.component.jsx
@@ -1,9 +1,101 @@
 import React from 'react';
+import { Collapse } from 'antd';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectReservationType , selectBookingType } from '../../redux/wizard/wizard.selectors';
 
 import './reservation-summery.styles.scss';
 
-const ReservationSummery = () => (
-    <div className="summeryWrap"></div>
+const { Panel } = Collapse;
+
+const ReservationSummery = ({ reservationType , bookingType }) => (
+    <div className="summeryWrap">
+
+        <div className="d-flex flex-wrap row">
+            <div className="summeryItem col-md-6 col-12">
+                <span className="label">Reservation Type</span>
+                <span className="value">{reservationType}</span>
+            </div>
+            <div className="summeryItem col-md-6 col-12">
+                <span className="label">Booking Type</span>
+                <span className="value">{bookingType}</span>
+            </div>
+        </div>
+
+        <div className="summeryItem">
+            <span className="label">Client Name</span>
+            <span className="value"></span>
+        </div>
+
+        <div className="summeryItem">
+            <span className="label">Trip Details</span>
+            <Collapse
+                expandIconPosition="right"
+            >
+                <Panel header="Pick Up Details" key="1" >
+                    
+                    <div className="summeryItem">
+                        <span className="label">Location</span>
+                        <span className="value"></span>
+                    </div>
+
+                    <div className="summeryItem">
+                        <span className="label">Date</span>
+                        <span className="value"></span>
+                    </div>
+
+                    <div className="summeryItem">
+                        <span className="label">Time</span>
+                        <span className="value"></span>
+                    </div>
+
+                </Panel>
+            </Collapse>
+        </div>
+
+        <div className="summeryItem">
+            <span className="label">Trip Details</span>
+            <Collapse
+                expandIconPosition="right"
+            >
+                <Panel header="Drop Off Details" key="2" >
+                    
+                    <div className="summeryItem">
+                        <span className="label">Location</span>
+                        <span className="value"></span>
+                    </div>
+
+                    <div className="summeryItem">
+                        <span className="label">Date</span>
+                        <span className="value"></span>
+                    </div>
+
+                    <div className="summeryItem">
+                        <span className="label">Time</span>
+                        <span className="value"></span>
+                    </div>
+
+                </Panel>
+            </Collapse>
+        </div>
+
+        <div className="summeryItem">
+            <span className="label">Passenger Details</span>
+            <span className="value"></span>
+        </div>
+
+        <div className="summeryItem">
+            <span className="label">Laguage Details</span>
+            <span className="value"></span>
+        </div>
+
+    </div>
 );
 
-export default ReservationSummery;
+const mapStateToProps = createStructuredSelector({
+    reservationType : selectReservationType,
+    bookingType : selectBookingType
+});
+
+export default connect(mapStateToProps)(ReservationSummery);
