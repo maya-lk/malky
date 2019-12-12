b4ea6b066cb8d47837b3bdc291f287c39e0aeb7b
hettipower <hettipower@gmail.com>
2019-12-12 19:56:07 +0530

    Create Vehicle item Component

---
+++ src/components/vehicle-item/vehicle-item.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './vehicle-item.styles.scss';
+
+const VehicleItem = ({ imageUrl , name , numberPlate , yard , ID }) => (
+    <div className="vehicleItem">
+        <input 
+            type="radio" 
+            name="vehicle"
+            id={`vehicle${ID}`}
+            value={ID}
+        />
+        <label class="vehicleLable d-flex" htmlFor={`vehicle${ID}`}>
+            <div className="vehicleImage">
+                <img src={imageUrl} alt={name}/>
+            </div>
+            <div className="vehicleContent">
+                <h4 className="name">{name}</h4>
+                <div className="numberPlate">{numberPlate}</div>
+                <div className="yard">YARD : {yard}</div>
+            </div>
+        </label>
+    </div>
+);
+
+export default VehicleItem;
