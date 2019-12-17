9937b44096efe418d80877346c417decd5eb932e
hettipower <hettipower@gmail.com>
2019-12-17 11:05:24 +0530

    Update Styles and dummy content

--- src/components/wizard-step-03/wizard-step-03.component.jsx
+++ src/components/wizard-step-03/wizard-step-03.component.jsx
@@ -20,7 +20,39 @@ const WizardStepThree = ({ history }) => {
             <div className="recomondations">
                 <h3>System Recommendations</h3>
                 <div className="vehicleList">
-                    <VehicleItem/>
+
+                    <VehicleItem
+                        imageUrl="https://auto.ndtvimg.com/car-images/medium/maruti-suzuki/baleno/maruti-suzuki-baleno.jpg"
+                        name="Maruti Suzuki Baleno"
+                        numberPlate="ABS - 10258"
+                        yard="Polhengoda"
+                        ID="125"
+                    />
+
+                    <VehicleItem
+                        imageUrl="https://activerent.hr/content/uploads/2016/04/Golf-VIInew-675x390.jpg"
+                        name="VW Golf VII Automatik"
+                        numberPlate="ABS - 10258"
+                        yard="Polhengoda"
+                        ID="145"
+                    />
+
+                    <VehicleItem
+                        imageUrl="https://auto.ndtvimg.com/car-images/medium/maruti-suzuki/baleno/maruti-suzuki-baleno.jpg"
+                        name="Maruti Suzuki Baleno"
+                        numberPlate="ABS - 10258"
+                        yard="Polhengoda"
+                        ID="526"
+                    />
+
+                    <VehicleItem
+                        imageUrl="https://activerent.hr/content/uploads/2016/04/Golf-VIInew-675x390.jpg"
+                        name="VW Golf VII Automatik"
+                        numberPlate="ABS - 10258"
+                        yard="Polhengoda"
+                        ID="874"
+                    />
+
                 </div>
             </div>
             <div className="searchVehicle">
@@ -73,13 +105,63 @@ const WizardStepThree = ({ history }) => {
                     </div>
 
                     <div className="vehicleSearchList">
-                        <VehicleItem/>
-                        <VehicleItem/>
-                        <VehicleItem/>
-                        <VehicleItem/>
-                        <VehicleItem/>
-                        <VehicleItem/>
-                        <VehicleItem/>
+
+                        <VehicleItem
+                            imageUrl="https://auto.ndtvimg.com/car-images/medium/maruti-suzuki/baleno/maruti-suzuki-baleno.jpg"
+                            name="Maruti Suzuki Baleno"
+                            numberPlate="ABS - 10258"
+                            yard="Polhengoda"
+                            ID="125"
+                        />
+
+                        <VehicleItem
+                            imageUrl="https://activerent.hr/content/uploads/2016/04/Golf-VIInew-675x390.jpg"
+                            name="VW Golf VII Automatik"
+                            numberPlate="ABS - 10258"
+                            yard="Polhengoda"
+                            ID="145"
+                        />
+
+                        <VehicleItem
+                            imageUrl="https://auto.ndtvimg.com/car-images/medium/maruti-suzuki/baleno/maruti-suzuki-baleno.jpg"
+                            name="Maruti Suzuki Baleno"
+                            numberPlate="ABS - 10258"
+                            yard="Polhengoda"
+                            ID="526"
+                        />
+
+                        <VehicleItem
+                            imageUrl="https://activerent.hr/content/uploads/2016/04/Golf-VIInew-675x390.jpg"
+                            name="VW Golf VII Automatik"
+                            numberPlate="ABS - 10258"
+                            yard="Polhengoda"
+                            ID="874"
+                        />
+
+                        <VehicleItem
+                            imageUrl="https://activerent.hr/content/uploads/2016/04/Golf-VIInew-675x390.jpg"
+                            name="VW Golf VII Automatik"
+                            numberPlate="ABS - 10258"
+                            yard="Polhengoda"
+                            ID="548"
+                        />
+
+                        <VehicleItem
+                            imageUrl="https://auto.ndtvimg.com/car-images/medium/maruti-suzuki/baleno/maruti-suzuki-baleno.jpg"
+                            name="Maruti Suzuki Baleno"
+                            numberPlate="ABS - 10258"
+                            yard="Polhengoda"
+                            ID="527"
+                        />
+
+                        <VehicleItem
+                            imageUrl="https://activerent.hr/content/uploads/2016/04/Golf-VIInew-675x390.jpg"
+                            name="VW Golf VII Automatik"
+                            numberPlate="ABS - 10258"
+                            yard="Polhengoda"
+                            ID="889"
+                        />
+
                     </div>
 
                     <span className="btn submitBtn" onClick={() => handleNextPage()}>Confirm and Proceed</span>
