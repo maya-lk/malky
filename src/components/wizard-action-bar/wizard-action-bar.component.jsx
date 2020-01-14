6bae7741f4b30baed61c5a00e36d5a8738b21466
hettipower <hettipower@gmail.com>
2020-01-14 09:46:26 +0530

    added availibility calender to wizards

--- src/components/wizard-action-bar/wizard-action-bar.component.jsx
+++ src/components/wizard-action-bar/wizard-action-bar.component.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Icon } from 'antd';
+import { Link } from 'react-router-dom';
 
 import './wizard-action-bar.styles.scss';
 
-const WizardActionBar = ({ reservationID }) => (
+const WizardActionBar = ({ reservationID , isWizard }) => (
     <div className="wizardActionBar d-flex justify-content-between">
         <div className="reservationID">Reservation ID : {reservationID}</div>
         <div className="wizardActions">
+            {
+                (isWizard)?
+                <Link className="btn" to="/availability-calender">View Availibility Calender</Link>
+                : ''
+            }
             <span className="btn"><Icon type="file-sync" /> Convert to an Estimation</span>
             <span className="btn"><Icon type="printer" /> Print Reservation</span>
         </div>
