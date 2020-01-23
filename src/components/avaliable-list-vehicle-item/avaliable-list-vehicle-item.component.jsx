96b9a82ca70985edc3f24f94cc48fc289ad20ab3
hettipower <hettipower@gmail.com>
2020-01-23 09:47:25 +0530

    Update availibity calender list component && create availiable list vehicle item component && create scroll function

---
+++ src/components/avaliable-list-vehicle-item/avaliable-list-vehicle-item.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './avaliable-list-vehicle-item.styles.scss';
+
+const AvaliableListVehicleItem = ({ refProp , imageUrl , name , vehiNo , yardName , perDay , totalFare , heightEle }) => (
+    <div ref={refProp} className="avaliableVehicleListItem" style={{ height : `${heightEle}px` }}>
+        <img src={imageUrl} alt={name} />
+        <div className="contWrap">
+            <h5>{name}</h5>
+            <div className="vehicleNo">{vehiNo}</div>
+            <div className="yardName">{yardName}</div>
+            <div className="perDay">Per day <span>{perDay}</span></div>
+            <div className="totalFare">Total fare <span>{totalFare}</span></div>
+        </div>
+    </div>
+);
+
+export default AvaliableListVehicleItem;
--- src/pages/availability-calender-list/availability-calender-list.component.jsx
+++ src/pages/availability-calender-list/availability-calender-list.component.jsx
@@ -10,6 +10,8 @@ import { selectListDates } from '../../redux/avalibility/avalibility.selectors';
 
 import { setListDates } from '../../redux/avalibility/avalibility.actions';
 
+import AvaliableListVehicleItem from '../../components/avaliable-list-vehicle-item/avaliable-list-vehicle-item.component';
+
 import './availability-calender-list.styles.scss';
 import '@fullcalendar/core/main.css';
 import '@fullcalendar/daygrid/main.css';
@@ -19,102 +21,179 @@ const { Option } = Select;
 const { RangePicker } = DatePicker;
 const { Search } = Input;
 
-const AvailabilityCalenderList = ({ setListDates , listDates }) => {
+const getDates = function(startDate, endDate) {
+    var dates = [],
+        currentDate = startDate,
+        addDays = function(days) {
+          var date = new Date(this.valueOf());
+          date.setDate(date.getDate() + days);
+          return date;
+        };
+    while (currentDate <= endDate) {
+        dates.push(currentDate);
+        currentDate = addDays.call(currentDate, 1);
+    }
+    return dates;
+}
 
-    const dateRangeChange = (date, dateString) => {
+class AvailabilityCalenderList extends React.Component {
+
+    ref = React.createRef();
+    scrollDates = React.createRef();
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            calItemHeight : '',
+            scrollLeftPosition : ''
+        }
+    }
+
+    componentDidMount() {
+        const { current } = this.ref;
+        this.setState({ calItemHeight : current.offsetHeight });
+    }
+
+    dateRangeChange = (date, dateString) => {
+        const { setListDates } = this.props;
         var dates = getDates(new Date(dateString[0]), new Date(dateString[1]));
         setListDates(dates);
     }
 
-    var getDates = function(startDate, endDate) {
-        var dates = [],
-            currentDate = startDate,
-            addDays = function(days) {
-              var date = new Date(this.valueOf());
-              date.setDate(date.getDate() + days);
-              return date;
-            };
-        while (currentDate <= endDate) {
-            dates.push(currentDate);
-            currentDate = addDays.call(currentDate, 1);
-        }
-        return dates;
-    };
-
-    const vehicleTypeChange = (value) => {
+    vehicleTypeChange = (value) => {
         console.log(`selected ${value}`);
     }
 
-    const onVehicleSearch = (value) => {
+    onVehicleSearch = (value) => {
         console.log(`search ${value}`);
     }
 
-    return(
-        <div className="availabilityCalenderWrap availabilityCalenderListWrap">
-            <div className="actionBar">
-                <div className="titleWrap">
-                    <h1>Availability Calender</h1>
-                    <div className="details">Showing results for</div>
-                </div>
-                <div className="otherActions">
-                    <div className="dateWrap">
-                        <RangePicker onChange={dateRangeChange} />
-                    </div>
-                    <div className="vehicleType">
-                        <Select
-                            showSearch
-                            placeholder="Vehicle Type"
-                            optionFilterProp="children"
-                            onChange={vehicleTypeChange}
-                            filterOption={(input, option) =>
-                                option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                            }
-                        >
+    render(){
+        const { listDates , allVehicles } = this.props;
+        const { calItemHeight , scrollLeftPosition } = this.state;
 
-                        </Select>
+        return(
+            <div className="availabilityCalenderWrap availabilityCalenderListWrap">
+                <div className="actionBar">
+                    <div className="titleWrap">
+                        <h1>Availability Calender</h1>
+                        <div className="details">Showing results for</div>
                     </div>
-                    <div className="btnsWrap">
-                        <Link className="btn active" to="/availability-calender-list"><i className="fas fa-bars"></i></Link>
-                        <Link className="btn" to="/availability-calender"><i className="fas fa-th-large"></i></Link>
-                    </div>
-                </div>
-            </div>
-            <div className="availabilityDetails">
-                
-                <div className="searchNCalenderDateWrap d-flex flex-wrap">
-                    <div className="searchWrap">
-                        <Search
-                            placeholder="Search vehicles"
-                            onSearch={value => onVehicleSearch(value)}
-                        />
-                    </div>
-                    <div className="calenderDatesWrap">
-                        <div className="calenderDatesInside" style={{ width : `${ listDates ? listDates.length * 90 : '' }px` }}>
-                            {
-                                (listDates) ?
-                                listDates.map( date => {
-                                    const weekdays = ['Sun' , 'Mon' , 'Tue' , 'Wen' , 'Thu' , 'Fri' , 'Sat'];
-                                    const listDate = date.getDate();
-                                    const listDateName = date.getDay();
-                                    return <div key={date} className="dateItem">
-                                        <span className="date">{listDate}</span>
-                                        <span className="weekDay">{weekdays[listDateName]}</span>
-                                    </div>;
-                                })
-                                : ''
-                            }
+                    <div className="otherActions">
+                        <div className="dateWrap">
+                            <RangePicker onChange={this.dateRangeChange} />
+                        </div>
+                        <div className="vehicleType">
+                            <Select
+                                showSearch
+                                placeholder="Vehicle Type"
+                                optionFilterProp="children"
+                                onChange={this.vehicleTypeChange}
+                                filterOption={(input, option) =>
+                                    option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                }
+                            >
+    
+                            </Select>
+                        </div>
+                        <div className="btnsWrap">
+                            <Link className="btn active" to="/availability-calender-list"><i className="fas fa-bars"></i></Link>
+                            <Link className="btn" to="/availability-calender"><i className="fas fa-th-large"></i></Link>
                         </div>
                     </div>
                 </div>
-                <div className="avalibilityDetailsWrap d-flex flex-wrap">
-                    <div className="vehiclesWrap"></div>
-                    <div className="avalibilityDetails"></div>
+                <div className="availabilityDetails">
+                    
+                    <div className="searchNCalenderDateWrap d-flex flex-wrap">
+                        <div className="searchWrap">
+                            <Search
+                                placeholder="Search vehicles"
+                                onSearch={value => this.onVehicleSearch(value)}
+                            />
+                        </div>
+                        <div
+                            ref={this.scrollDates}
+                            className="calenderDatesWrap"
+                            onScroll={ () => this.setState({ scrollLeftPosition : this.scrollDates.current.scrollLeft }) }
+                        >
+                            <div className="calenderDatesInside" style={{ width : `${ listDates ? listDates.length * 90 : '' }px` }}>
+                                {
+                                    (listDates) ?
+                                    listDates.map( date => {
+                                        const weekdays = ['Sun' , 'Mon' , 'Tue' , 'Wen' , 'Thu' , 'Fri' , 'Sat'];
+                                        const listDate = date.getDate();
+                                        const listDateName = date.getDay();
+                                        return <div key={date} className="dateItem">
+                                            <span className="date">{listDate}</span>
+                                            <span className="weekDay">{weekdays[listDateName]}</span>
+                                        </div>;
+                                    })
+                                    : ''
+                                }
+                            </div>
+                        </div>
+                    </div>
+                    <div className="avalibilityDetailsWrap d-flex flex-wrap">
+                        <div className="vehiclesWrap">
+                            {
+                                (allVehicles) ?
+                                allVehicles
+                                .map( 
+                                    vehicle => 
+                                    <AvaliableListVehicleItem
+                                        refProp={this.ref}
+                                        key={vehicle.name}
+                                        imageUrl={vehicle.pic}
+                                        name={vehicle.name}
+                                        yardName="Kirulapana"
+                                        vehiNo=""
+                                        perDay=""
+                                        totalFare=""
+                                        heightEle={`${calItemHeight}px`}
+                                    />
+                                )
+                                : ''
+                            }
+                        </div>
+                        <div className="avalibilityDetails">
+                            <div 
+                                className="avalibilityDetailsInside" 
+                                style={{ 
+                                    width : `${ listDates ? listDates.length * 90 : '' }px`,
+                                    transform : `translateX(-${scrollLeftPosition}px)`
+                                }}
+                            >
+                                {
+                                    (allVehicles) ?
+                                    allVehicles
+                                    .map( 
+                                        vehicle => 
+                                        <div
+                                            key={vehicle.name} 
+                                            className="availibilityCalTr"
+                                            style={{ height : `${calItemHeight}px` }}
+                                        >
+                                            {
+                                                (listDates) ?
+                                                listDates.map( date => {
+                                                    return <div key={date} className="availibityCalTd"></div>;
+                                                })
+                                                : ''
+                                            }
+                                        </div>
+                                    )
+                                    : ''
+                                }
+                            </div>
+                        </div>
+                    </div>
+    
                 </div>
-
             </div>
-        </div>
-    )
-};
+        )
+    }
+}
 
 const mapStateToProps = createStructuredSelector({
     allVehicles : selectAllVehicles,
